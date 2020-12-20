9ebe68d4d2d5dc71de143da57b181064115ac297
alazir <akrachenko@nikti.org.ua>
2020-12-20 09:38:50 +0200

    Добавили проект на Github

---
+++ Shared/gittestApp.swift
@@ -0,0 +1,18 @@
+//
+//  gittestApp.swift
+//  Shared
+//
+//  Created by Александр Кравченко on 20.12.2020.
+//
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
